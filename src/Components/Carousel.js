b55d5baa6542674bd2a0887246c637103d164d54
Jeremiah Long <v-jelong@microsoft.com>
2018-12-10 14:55:21 -0800

    Formatting and var change

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import {
-  Carousel,
-  CarouselItem,
-  CarouselControl,
-  CarouselIndicators,
-  CarouselCaption
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators,
+    CarouselCaption
 } from 'reactstrap';
 
 import * as API from './../Utilities/ApiResult';
@@ -12,89 +12,89 @@ import * as API from './../Utilities/ApiResult';
 class CatCarousel extends Component {
     constructor(props) {
     super(props);
-    this.state = { testCats: [],  activeIndex: 0 };
-    this.next = this.next.bind(this);
-    this.previous = this.previous.bind(this);
-    this.goToIndex = this.goToIndex.bind(this);
-    this.onExiting = this.onExiting.bind(this);
-    this.onExited = this.onExited.bind(this);
+        this.state = { testCats: [],  activeIndex: 0 };
+        this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
+        this.goToIndex = this.goToIndex.bind(this);
+        this.onExiting = this.onExiting.bind(this);
+        this.onExited = this.onExited.bind(this);
     }
 
     componentDidMount(){
-    API.getCats().then(res => {
-        this.setState({ 
-            testCats : res.data 
-        }, () => console.log(this.state.testCats));
-    });
+        API.getCats().then(res => {
+            this.setState({ 
+                testCats : res.data 
+            }, () => console.log(this.state.testCats));
+        });
     }
 
     onExiting() {
-    this.animating = true;
+        this.animating = true;
     }
 
     onExited() {
-    this.animating = false;
+        this.animating = false;
     }
 
     next() {
-    if (this.animating) return;
-    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
-    this.setState({ activeIndex: nextIndex });
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === this.state.testCats.length - 1 ? 0 : this.state.activeIndex + 1;
+        this.setState({ activeIndex: nextIndex });
     }
 
     previous() {
-    if (this.animating) return;
-    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
-    this.setState({ activeIndex: nextIndex });
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === 0 ? this.state.textCats.length - 1 : this.state.activeIndex - 1;
+        this.setState({ activeIndex: nextIndex });
     }
 
     goToIndex(newIndex) {
-    if (this.animating) return;
-    this.setState({ activeIndex: newIndex });
+        if (this.animating) return;
+        this.setState({ activeIndex: newIndex });
     }
 
     render() {
-    const { activeIndex } = this.state;
+        const { activeIndex } = this.state;
 
-    const slides = this.state.testCats.map((cat) => {
+        const slides = this.state.testCats.map((cat) => {
+            return (
+                <CarouselItem
+                className="custom-tag"
+                tag="div"
+                key={cat.id}
+                onExiting={this.onExiting}
+                onExited={this.onExited}
+                >
+                    <img src={cat.image.url} alt={cat.image.id} />
+                    <CarouselCaption captionText={cat.sub_id} captionHeader={cat.image.id} />
+                </CarouselItem>
+            )
+        })
+
         return (
-            <CarouselItem
-            className="custom-tag"
-            tag="div"
-            key={cat.id}
-            onExiting={this.onExiting}
-            onExited={this.onExited}
-            >
-                <img src={cat.image.url} alt={cat.image.id} />
-                <CarouselCaption className="text-danger" captionText={cat.sub_id} captionHeader={cat.image.id} />
-            </CarouselItem>
-        )
-    })
+            <div>
+                <style>
+                    {
+                    `.custom-tag {
+                        max-width: 100%;
+                        height: 500px;
+                        background: black;
+                        }`
+                    }
+                </style>
 
-    return (
-        <div>
-        <style>
-            {
-            `.custom-tag {
-                max-width: 100%;
-                height: 500px;
-                background: black;
-                }`
-            }
-        </style>
-
-        <Carousel
-            activeIndex={activeIndex}
-            next={this.next}
-            previous={this.previous}
-        >
-            <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
-            {slides}
-            <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
-            <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
-        </Carousel>
-        </div>
-    );
+                <Carousel
+                    activeIndex={activeIndex}
+                    next={this.next}
+                    previous={this.previous}
+                >
+                    <CarouselIndicators items={this.state.testCats} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+                    {slides}
+                    <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+                    <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+                </Carousel>
+            </div>
+        );
     }
 }
 
