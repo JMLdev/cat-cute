950e40e320b6863b4f958b4b84b9a1efa1b4afb2
Jeremiah Long <v-jelong@microsoft.com>
2018-12-10 14:58:08 -0800

    data import complete

--- src/Utilities/ApiResult.js
+++ src/Utilities/ApiResult.js
@@ -13,37 +13,3 @@ export function  getCats(){
     // GET THE LIST OF CATS 
     return axios.get(apiUrl)
 }
-
-// export default class ApiResult extends Component {
-
-//     constructor(props){
-//         super(props);
-//         this.state = {
-//             cats : []
-//         }
-//     }
-
-//     componentDidMount(){
-//         axios.defaults.headers = {
-//             'Content-Type': 'application/json',
-//             'x-api-key': 'd70ced34-3597-4aa8-ad8c-2861cbe8452c'
-//         }
-
-//         // get the list of cats 
-//         axios.get(apiUrl).then(res => {
-//             console.log(res);
-//             this.setState({
-//                 cats : res.data
-//             });
-//         })
-//     }
-
-//     render() {
-//         return (
-//             <ul>
-//                 { this.state.cats.map(cat => <li key={cat.image.id}><img src={cat.image.url} alt={cat.image.id} /></li>)}
-//             </ul>        );
-//       }
-// }
-
-
