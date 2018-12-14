158670d3946dbb757124c361ae41efccbcbc9681
Jeremiah Long <v-jelong@microsoft.com>
2018-12-13 17:48:56 -0800

    Updated function on list page to push votes to list... not really a react way of doing it

--- src/List.js
+++ src/List.js
@@ -9,20 +9,51 @@ class List extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { testCats: [] };
+        this.state = { 
+            theCats: [],
+            listCats: {},
+            listVotes: {}
+        };
     }
 
-    componentDidMount(){
+    componentDidMount() {
         API.getCats().then(res => {
             this.setState({ 
-                testCats : res.data 
+                theCats : res.data,
+                listCats: res.data
+            });
+        });
+        API.getVotes().then(res => {
+            this.setState({
+                listVotes: res.data
             });
         });
     }
 
     render() {
-
-        const kitties = this.state.testCats.map((cat) => {
+        let prettyKitties = () =>{
+            // loop the top level objects
+            for (let key in this.state.listVotes) {
+                // loop each key
+                for (let subkey in this.state.listVotes) {
+                    if (this.state.listVotes.hasOwnProperty(subkey)) {
+                        if (typeof this.state.listVotes[key].value !== 'undefined') {
+                            let id = this.state.listVotes[key].image_id;
+                            let vote = this.state.listVotes[key].value;
+                            for (let key in this.state.listCats) {
+                                for (let subkey in this.state.listCats) {
+                                    if (this.state.listCats[key].image_id === id) {
+                                        this.state.listCats[key].value = vote;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            console.log(this.state.listCats);
+        }
+        const kitties = this.state.theCats.map((cat) => {
             return (
                 <ListGroupItem key={cat.image.id}>
                     <img src={cat.image.url} alt={cat.image.id} style={{paddingRight: '20px', display: 'inline-block', maxWidth: '150px'}} />
@@ -32,6 +63,7 @@ class List extends Component {
         })
         return (
             <React.Fragment>
+                {prettyKitties()}
                 <Graph />
                 <Row>
                     <Col>
--- src/Utilities/ApiResult.js
+++ src/Utilities/ApiResult.js
@@ -4,26 +4,35 @@ const apiUrl = 'https://api.thecatapi.com/v1/favourites?sub_id=imfm4j'
 const apiVoteUrl = 'https://api.thecatapi.com/v1/votes'
 const apiGetVoteUrl = 'https://api.thecatapi.com/v1/votes?sub_id=imfm4j'
 
+const returnHeaders = () => {
+    return (
+        axios.defaults.headers = {
+            'CONTENT-TYPE': 'APPLICATION/JSON',
+            'X-API-KEY': 'D70CED34-3597-4AA8-AD8C-2861CBE8452C'
+        }
+    )
+}
+
 export function  getCats(){
-    axios.defaults.headers = {
-        'CONTENT-TYPE': 'APPLICATION/JSON',
-        'X-API-KEY': 'D70CED34-3597-4AA8-AD8C-2861CBE8452C'
-    }
+    returnHeaders();
 
     // GET THE LIST OF CATS 
     return axios.get(apiUrl)
+
+    // returns 
+    // {
+    //     created_at: "2018-12-06T18:19:12.000Z",
+    //     id: 4124,
+    //     image: {id: "bod", url: "https://cdn2.thecatapi.com/images/bod.jpg"},
+    //     image_id: "bod",
+    //     sub_id: "imfm4j",
+    //     user_id: "imfm4j"
+    // }
 }
 
-export function findCute(cat_id) {
-    axios.get(apiVoteUrl + cat_id).then(res => {
-    })
-}
 // Post Api for Voting
 export function isCute(image_id, sub_id, yes_or_no){
-    axios.defaults.headers = {
-        'CONTENT-TYPE': 'APPLICATION/JSON',
-        'X-API-KEY': 'D70CED34-3597-4AA8-AD8C-2861CBE8452C'
-    }
+    returnHeaders();
 
     const cat = {
         "image_id": image_id,
@@ -31,16 +40,11 @@ export function isCute(image_id, sub_id, yes_or_no){
         "value": yes_or_no
       };
 
-    axios.post(apiVoteUrl, cat).then(res => {
-        console.log(res.data);
-    })
+    axios.post(apiVoteUrl, cat);
 }
 
-export function  getVotes(){
-    axios.defaults.headers = {
-        'CONTENT-TYPE': 'APPLICATION/JSON',
-        'X-API-KEY': 'D70CED34-3597-4AA8-AD8C-2861CBE8452C'
-    }
+export function getVotes() {
+    returnHeaders();
 
     // GET THE VOTES 
     /* Result includes following parameters: (example)
@@ -55,8 +59,4 @@ export function  getVotes(){
          We need to use value for vote (1 or 0) and image_id and sub_id to map with the testcats[] list
     */
     return axios.get(apiGetVoteUrl)
-}
-
-export function mappingVotes(){
-   
 }
