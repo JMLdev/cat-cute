bf0a745182e1785cc190e2763e35e4887c01cd7a
Jeremiah Long <jml@jmldev.com>
2018-12-26 10:24:57 -0800

    Started moving stuff to app. Not finished, pushing for collaboration

--- src/App.js
+++ src/App.js
@@ -5,8 +5,47 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Home from './Home';
 import Rate from './Rate';
 import List from './List';
+import * as API from './Utilities/ApiResult';
 
 class App extends Component {
+
+    componentWillMount() {
+        API.getCats().then(res => {
+            this.setState({ 
+                theCats : res.data,
+                listCats: res.data
+            });
+        });
+        API.getVotes().then(res => {
+            this.setState({
+                listVotes: res.data
+            });
+        }).then(() => {
+            let catList = Object.assign({}, this.state.listCats)
+            // loop the top level objects
+            for (let key in this.state.listVotes) {
+                // loop each key
+                for (let subkey in this.state.listVotes) {
+                    if (this.state.listVotes.hasOwnProperty(subkey)) {
+                        if (typeof this.state.listVotes[key].value !== 'undefined') {
+                            let id = this.state.listVotes[key].image_id;
+                            let vote = this.state.listVotes[key].value;
+                            for (let key in catList) {
+                                // eslint-disable-next-line
+                                for (let subkey in catList) {
+                                    if (catList[key].image_id === id) {
+                                        catList[key].value = vote;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            this.setState({ listCats : catList })
+        })
+    }
+
     render() {
         return (
             <Switch>
--- src/Components/Graph.js
+++ src/Components/Graph.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Row, Col, Progress } from 'reactstrap';
 
 class Graph extends Component {
-
+    // takes a prop of 0 or 1. 1 is cute, 0 is not
+    // returns the number of cute or not as specified by prop when calling 
     getCute(cuteOrCrap) {
         let cute = 0;
         for (let key in this.props.list) {
@@ -15,10 +16,12 @@ class Graph extends Component {
         return cute;
     }
 
+    // returns the number of cute cats out of the list. 1 is cute, 0 is not
     returnCute() {
         return Math.floor(this.getCute(1) / (this.getCute(1) + this.getCute(0)) * 100)
     }
 
+    // returns the number of crappy cats
     returnCrap() {
         return Math.floor(this.getCute(0) / (this.getCute(1) + this.getCute(0)) * 100)
     }
