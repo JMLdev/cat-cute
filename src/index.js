a0c2d77d10c89f8d10ef271bc1e012aa47317268
Jeremiah Long <jml@jmldev.com>
2019-01-04 13:52:44 -0800

    removing react-router-dom, going with react-router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Router } from 'react-router-dom';
+import { Router } from 'react-router';
 import createHashHistory from 'history/createHashHistory';
 import * as serviceWorker from './serviceWorker';
 import { Container } from 'reactstrap';
